4ac258f0da862751e666a8a9d785376aaa5844bd
Tatiana Sierkova <tanuha_86@ukr.net>
2021-11-04 02:00:58 +0200

    pppppppppppppppp

--- js/mobile.js
+++ js/mobile.js
@@ -2,7 +2,7 @@
  const refs = {
     openMobileBtn: document.querySelector('[data-mobile-open]'),
     closeMobileBtn: document.querySelector('[data-mobile-close]'),
-    mobile: document.querySelector('[data-modal]'),
+    mobile: document.querySelector('[data-mobile]'),
  };
 
  refs.openMobileBtn.addEventListener('click', toggleMobile);
