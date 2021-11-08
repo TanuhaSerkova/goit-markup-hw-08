86b8668664d1947d7eba9b680539d335dfb4e137
Tatiana Sierkova <tanuha_86@ukr.net>
2021-11-08 14:40:19 +0200

    mobile js

--- js/mobile.js
+++ js/mobile.js
@@ -3,6 +3,7 @@
     openMobileBtn: document.querySelector('[data-mobile-open]'),
     closeMobileBtn: document.querySelector('[data-mobile-close]'),
     mobile: document.querySelector('[data-mobile]'),
+    body: document.querySelector('body'),
  };
 
  refs.openMobileBtn.addEventListener('click', toggleMobile);
@@ -10,5 +11,6 @@
 
  function toggleMobile() {
     refs.mobile.classList.toggle('is-hidden');
+    !refs.mobile.classList.contains('is-hidden') ? refs.body.style.overflow='hidden' : refs.body.style.removeProperty('overflow');
  }
 })();
